aceaeba42312b7c353c36d4cd04ee8a5f38f01a9
Bencio09 <lorebencini13@outlook.com>
2020-12-15 11:11:28 +0100

    Scritto programma

--- Test.java
+++ Test.java
@@ -15,7 +15,9 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
     
 }
